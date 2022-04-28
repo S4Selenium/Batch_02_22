d414df9cd60f4d219b5e862b848b96b70ae5349d
S4Selenium <47279496+Nithinps021@users.noreply.github.com>
2022-04-29 02:12:37 +0530

    LiveProj

---
+++ PayRoll/src/main/java/com/payroll/pageobjects/HomePage.java
@@ -0,0 +1,6 @@
+package com.payroll.pageobjects;
+
+public class HomePage {
+
+	
+}
---
+++ PayRoll/src/main/java/com/payroll/pageobjects/LoginPage.java
@@ -0,0 +1,7 @@
+package com.payroll.pageobjects;
+//POM using PageObject Factory
+public class LoginPage  {
+	
+	
+	
+}
